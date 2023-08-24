1e403b6dc721f7143a161186838acdae89ae3b27
125749979+Jhoan5@users.noreply.github.com <sebastiandiaz4757@hotmail.com>
2023-08-24 10:35:59 -0500

    creo clase Input e inicio constructor con valores de un input y propiedad de retorno .div

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+class Input{
+    constructor(type,name,id,value){
+        this.type = type;
+        this.name = name;
+        this.id = id;
+        this.value = value;
+        this.div = this.getDiv();
+    }
+}
